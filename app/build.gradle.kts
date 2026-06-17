60bb6116681af9e838a414fd1d2286acb43626aa
DOHEX <dohex@outlook.com>
2026-06-17 22:37:05 +0800

    chore: bump version to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dohex.hyperrose"
         minSdk = 35
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
     signingConfigs {
         if (hasReleaseSigningConfig) {
